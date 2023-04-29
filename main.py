3c707f213ec98e49ed9e19dc2b8a7d505c29f2d4
ツ Ⱡσƀσ <lobocode@gmail.com>
2023-04-29 20:03:27 -0300

    Restructure main with a helper

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 # main.py
+import os
+from helper import parse_arguments
 from transcript_downloader import TranscriptDownloader
 from transcript_processor import TranscriptProcessor
 
-video_id = 'QOIJeIbxquM'
-output_file = "transcript.txt"
+def main():
+    # Parse command line arguments
+    args = parse_arguments()
 
-# Instantiate an object of the TranscriptDownloader class
-downloader = TranscriptDownloader(video_id)
+    # Download transcript
+    downloader = TranscriptDownloader(args.download)
+    downloader.save_transcript(args.output)
 
-# Call the download_transcript method to get the transcript
-transcript = downloader.download_transcript()
+    # Process transcript
+    if args.format or args.minify:
+        with open(args.output, 'r', encoding='utf-8') as f:
+            raw_transcript = f.readlines()
 
-# Instantiate an object of the TranscriptProcessor class
-processor = TranscriptProcessor(transcript)
+        processor = TranscriptProcessor(raw_transcript)
 
-# Format the transcript and save it to a new file
-formatted_transcript = processor.format_transcript()
-with open(f"{output_file.split('.')[0]}_formatted.txt", "w", encoding="utf-8") as
+        if args.format:
+            formatted_transcript = processor.format_transcript()
+            output_file = os.path.splitext(args.output)[0] + '_formatted.txt'
+
+        if args.minify:
+            minified_transcript = processor.minify_transcript()
+            output_file = os.path.splitext(args.output)[0] + '_minified.txt'
+
+        with open(output_file, 'w', encoding='utf-8') as f:
+            if args.format:
+                for line in formatted_transcript:
+                    f.write(f"{line}\n")
+
+            if args.minify:
+                f.write(minified_transcript)
+
+        print(f"Processed transcript saved in {output_file}.")
+
+if __name__ == '__main__':
+    main()
