2845413530c13697a472f65cec445ded83966c82
Vitor Lobo Ramos <lobocode@gmail.com>
2023-05-09 10:09:02 -0300

    metavar, default, pattern and description

--- src/helper.py
+++ src/helper.py
@@ -16,6 +16,6 @@ def parse_arguments():
     # Optional arguments
     parser.add_argument('-f', '--format', action='store_true', help='format the transcript by removing unwanted characters like numbers and punctuation')
     parser.add_argument('-m', '--minify', action='store_true', help='minify the transcript by removing extra whitespace and line breaks')
-    parser.add_argument('-l', help='specify a list of language in a descending priority', nargs='+')
+    parser.add_argument('-l', '--language', metavar='LANGUAGE_CODE', default=['en'], nargs='+', help='language codes of the transcript, default is "en"')
 
     return parser.parse_args()
