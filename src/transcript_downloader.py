6b3aa1e458bdd1bf766ce98615feeb0776c59a56
Vitor Lobo Ramos <lobocode@gmail.com>
2023-05-09 10:17:19 -0300

    add exceptions if lang does not exist

--- src/transcript_downloader.py
+++ src/transcript_downloader.py
@@ -1,25 +1,35 @@
 # transcript_downloader.py
 from youtube_transcript_api import YouTubeTranscriptApi
 
+class TranscriptNotAvailable(Exception):
+    pass
+
+class TranscriptDownloadError(Exception): 
+    pass
+
 class TranscriptDownloader:
-    """
-    A class that can download transcripts of YouTube videos.
-    """
     def __init__(self, video_id, language_codes=None):
         self.video_id = video_id
         self.language_codes = language_codes or ['en']
 
     def download_transcript(self):
-        """
-        Download the transcript for a YouTube video.
-
-        Returns:
-            List[Dict[str, Union[str, float, int]]]: A list of dictionaries representing each
-            part of the transcript, with 'text', 'start' and 'duration' keys.
-        """
         try:
             transcript_list = YouTubeTranscriptApi.get_transcript(self.video_id, languages=self.language_codes)
             return transcript_list
         except Exception as e:
-            print(f"Error getting transcript: {str(e)}")
-            return None
+            error_message = str(e)
+            if "Could not retrieve a transcript for the video" in error_message:
+                raise TranscriptDownloadError("Transcript not available for this video. Try another video ID or language code.")
+            raise TranscriptNotAvailable(error_message)
+
+def download_and_process_transcript(args):
+    downloader = TranscriptDownloader(args.download, args.language)
+    try:
+        transcript_list = downloader.download_transcript()
+        return transcript_list, downloader
+    except TranscriptNotAvailable as e:
+        print(f"Error getting transcript: {str(e)}")
+        return None
+    except TranscriptDownloadError as e:
+        print(f"Error downloading transcript: {str(e)}")
+        return None
