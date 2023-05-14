b2d384c9fedbf2151195e9a6db63f21403941357
Vitor Lobo Ramos <lobocode@gmail.com>
2023-05-13 23:04:53 -0300

    ValueError exception handling

--- src/markai.py
+++ src/markai.py
@@ -2,10 +2,20 @@ from pychatsonic.chat import ChatSonic
 import os
 
 def process_with_ai(args, formatted_transcript):
-    output_file_ai = os.path.splitext(args.output)[0] + '_ai.txt'
-    chat = ChatSonic("add-your-api-key", f"{args.language}")
-    ai_transcript = chat.ask(f"Apenas corrija o texto a seguir: {' '.join(formatted_transcript)}")
+    api_key = "add-your-api-key"
 
+    if not api_key or api_key == "add-your-api-key":
+        print("Invalid API key. Please provide a valid API key.")
+        return
+
+    output_file_ai = os.path.splitext(args.output)[0] + '_ai.txt'
+    try:
+        chat = ChatSonic(api_key, f"{args.language}")
+        ai_transcript = chat.ask(f"Just correct the following text in the language {args.language}: {' '.join(formatted_transcript)}")
+    except Exception as e:
+        print("Error processing with AI: {}".format(e))
+        return
+    
     with open(output_file_ai, 'w', encoding='utf-8') as file:
         file.write(ai_transcript)
     
