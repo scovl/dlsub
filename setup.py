069fcb373d0dbdf4490d08bdc235e0fff9bb3deb
Vitor Lobo Ramos <lobocode@gmail.com>
2023-05-15 12:33:16 -0300

    update setuptools

--- setup.py
+++ setup.py
@@ -2,23 +2,33 @@ from setuptools import setup, find_packages
 
 setup(
     name='dlsub',
-    version='1.0.0',
-    description='A command line tool for downloading transcripts of YouTube videos',
-    author='Your Name',
-    author_email='your.email@example.com',
-    url='https://github.com/yourusername/dlsub',
+    version='0.0.1',
+    description='A tool for downloading and processing YouTube video transcripts.',
+    author='Vitor Lobo Ramos',
+    author_email='lobocode@gmail.com',
+    url='https://github.com/lobocode/dlsub',
     packages=find_packages(),
     install_requires=[
-        'youtube_transcript_api'
+        'enelvo==0.15',
+        'pychatsonic==0.1',
+        'setuptools==44.1.1',
+        'spacy==3.5.2',
+        'tqdm==4.65.0',
+        'youtube_transcript_api==0.6.0',
     ],
     entry_points={
         'console_scripts': [
-            'dlsub=dlsub.main:main'
-        ]
+            'dlsub=dlsub:main',
+        ],
     },
     classifiers=[
-        'Programming Language :: Python :: 3',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
